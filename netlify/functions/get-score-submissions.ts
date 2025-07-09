3e2af6e61e5c4c3c009c4f74e0ae8dfa93e27d00
BrianKWelch <briankwelch70@gmail.com>
2025-07-09 19:36:52 -0400

    Added all the git-s

---
+++ netlify/functions/get-score-submissions.ts
@@ -0,0 +1,19 @@
+import { Handler } from '@netlify/functions';
+import { neon } from '@neondatabase/serverless';
+
+const sql = neon(process.env.NETLIFY_DATABASE_URL!);
+
+export const handler: Handler = async () => {
+  try {
+    const result = await sql`SELECT * FROM score_submissions ORDER BY id`;
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+  } catch (err: any) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
